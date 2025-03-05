2d32d9789654e7e2766f1dbe28e3210f2a66091e
TasikIslam <159548447+TasikIslam@users.noreply.github.com>
2025-03-05 21:50:53 +0600

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -4,34 +4,29 @@ apt update -y && apt upgrade -y
 
 apt install grub2 wimtools ntfs-3g -y
 
-#Get the disk size in GB and convert to MB
+# Get the disk size in GB and convert to MB
 disk_size_gb=$(parted /dev/sda --script print | awk '/^Disk \/dev\/sda:/ {print int($3)}')
 disk_size_mb=$((disk_size_gb * 1024))
 
-#Calculate partition size (25% of total size)
+# Calculate partition size (25% of total size)
 part_size_mb=$((disk_size_mb / 4))
 
-#Create GPT partition table
+# Create GPT partition table
 parted /dev/sda --script -- mklabel gpt
 
-#Create two partitions
+# Create two partitions
 parted /dev/sda --script -- mkpart primary ntfs 1MB ${part_size_mb}MB
 parted /dev/sda --script -- mkpart primary ntfs ${part_size_mb}MB $((2 * part_size_mb))MB
 
-#Inform kernel of partition table changes
+# Inform kernel of partition table changes
 partprobe /dev/sda
-
 sleep 30
-
 partprobe /dev/sda
-
 sleep 30
-
 partprobe /dev/sda
-
 sleep 30 
 
-#Format the partitions
+# Format the partitions
 mkfs.ntfs -f /dev/sda1
 mkfs.ntfs -f /dev/sda2
 
@@ -41,7 +36,7 @@ echo -e "r\ng\np\nw\nY\n" | gdisk /dev/sda
 
 mount /dev/sda1 /mnt
 
-#Prepare directory for the Windows disk
+# Prepare directory for the Windows disk
 cd ~
 mkdir windisk
 
@@ -49,14 +44,14 @@ mount /dev/sda2 windisk
 
 grub-install --root-directory=/mnt /dev/sda
 
-#Edit GRUB configuration
+# Edit GRUB configuration
 cd /mnt/boot/grub
 cat <<EOF > grub.cfg
 menuentry "windows installer" {
-	insmod ntfs
-	search --set=root --file=/bootmgr
-	ntldr /bootmgr
-	boot
+    insmod ntfs
+    search --set=root --file=/bootmgr
+    ntldr /bootmgr
+    boot
 }
 EOF
 
@@ -64,28 +59,13 @@ cd /root/windisk
 
 mkdir winfile
 
-wget -O 20348.1.210507-1500.fe_release_amd64fre_SERVER_LOF_PACKAGES_OEM.iso --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" https://go.microsoft.com/fwlink/p/?linkid=2195333
+# Download Windows Server 2022 ISO without user-agent
+wget -O Windows_SERVER_2022_NTLite.iso "https://www.dropbox.com/scl/fi/kjvjlmhbt8fbwa9zxmke2/Windows_SERVER_2022_NTLite.iso?rlkey=2qrb3egcnec7wnt3wqrrk50rl&st=t69g63uc&dl=1"
 
-mount -o loop 20348.1.210507-1500.fe_release_amd64fre_SERVER_LOF_PACKAGES_OEM.iso winfile
+mount -o loop Windows_SERVER_2022_NTLite.iso winfile
 
 rsync -avz --progress winfile/* /mnt
 
 umount winfile
-
-wget -O virtio.iso https://bit.ly/4d1g7Ht
-
-mount -o loop virtio.iso winfile
-
-mkdir /mnt/sources/virtio
-
-rsync -avz --progress winfile/* /mnt/sources/virtio
-
-cd /mnt/sources
-
-touch cmd.txt
-
-echo 'add virtio /virtio_drivers' >> cmd.txt
-
-wimlib-imagex update boot.wim 2 < cmd.txt
 
 reboot
