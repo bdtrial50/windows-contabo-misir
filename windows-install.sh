759fe281c56038a0903b035b7d82eac379d9c4a4
bdtrial50 <bdtrial50@gmail.com>
2025-03-19 15:46:59 +0600

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+# Update system and install required packages
 apt update -y && apt upgrade -y
-
 apt install grub2 wimtools ntfs-3g -y
 
 # Get the disk size in GB and convert to MB
@@ -20,34 +20,40 @@ parted /dev/sda --script -- mkpart primary ntfs ${part_size_mb}MB $((2 * part_si
 
 # Inform kernel of partition table changes
 partprobe /dev/sda
-sleep 30
-partprobe /dev/sda
-sleep 30
-partprobe /dev/sda
-sleep 30 
 
-# Format the partitions
+sleep 30
+
+# Reapply partprobe after a short delay
+partprobe /dev/sda
+sleep 30
+
+partprobe /dev/sda
+sleep 30
+
+# Format the partitions as NTFS
 mkfs.ntfs -f /dev/sda1
 mkfs.ntfs -f /dev/sda2
 
 echo "NTFS partitions created"
 
+# Use gdisk to update the partition table
 echo -e "r\ng\np\nw\nY\n" | gdisk /dev/sda
 
+# Mount the first partition
 mount /dev/sda1 /mnt
 
 # Prepare directory for the Windows disk
 cd ~
 mkdir windisk
-
 mount /dev/sda2 windisk
 
+# Install GRUB bootloader
 grub-install --root-directory=/mnt /dev/sda
 
-# Edit GRUB configuration
+# Edit GRUB configuration to add Windows boot entry
 cd /mnt/boot/grub
 cat <<EOF > grub.cfg
-menuentry "windows installer" {
+menuentry "Windows installer" {
     insmod ntfs
     search --set=root --file=/bootmgr
     ntldr /bootmgr
@@ -55,17 +61,38 @@ menuentry "windows installer" {
 }
 EOF
 
+# Download Windows ISO if not already downloaded
 cd /root/windisk
 
+if [ ! -f win10.iso ]; then
+    echo "Downloading Windows 10 ISO..."
+    wget -O win10.iso --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" https://bit.ly/3UGzNcB
+fi
+
+# Mount the ISO file and copy its contents to the partition
 mkdir winfile
-
-# Download Windows Server 2022 ISO without user-agent
-wget -O Windows_SERVER_2022_NTLite.iso "https://www.dropbox.com/scl/fi/kjvjlmhbt8fbwa9zxmke2/Windows_SERVER_2022_NTLite.iso?rlkey=2qrb3egcnec7wnt3wqrrk50rl&st=t69g63uc&dl=1"
-
-mount -o loop Windows_SERVER_2022_NTLite.iso winfile
-
+mount -o loop win10.iso winfile
 rsync -avz --progress winfile/* /mnt
 
+# Unmount the Windows ISO
 umount winfile
 
+# Download Virtio drivers ISO
+if [ ! -f virtio.iso ]; then
+    echo "Downloading Virtio drivers ISO..."
+    wget -O virtio.iso https://bit.ly/4d1g7Ht
+fi
+
+# Mount Virtio drivers ISO and copy its contents
+mount -o loop virtio.iso winfile
+mkdir /mnt/sources/virtio
+rsync -avz --progress winfile/* /mnt/sources/virtio
+
+# Update boot.wim with Virtio drivers
+cd /mnt/sources
+touch cmd.txt
+echo 'add virtio /virtio_drivers' >> cmd.txt
+wimlib-imagex update boot.wim 2 < cmd.txt
+
+# Reboot the system
 reboot
