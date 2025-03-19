143342562d47892d1455c9bbcb2df55ae10cb2ce
bdtrial50 <bdtrial50@gmail.com>
2025-03-19 17:03:02 +0600

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+# Update system and install required packages
 apt update -y && apt upgrade -y
-
 apt install grub2 wimtools ntfs-3g -y
 
 # Get the disk size in GB and convert to MB
@@ -12,44 +12,42 @@ disk_size_mb=$((disk_size_gb * 1024))
 part_size_mb=$((disk_size_mb / 4))
 
 # Create GPT partition table
-parted /dev/sda --script -- mklabel gpt
+parted /dev/sda --script mklabel gpt
 
-# Create two partitions
-parted /dev/sda --script -- mkpart primary ntfs 1MB ${part_size_mb}MB
-parted /dev/sda --script -- mkpart primary ntfs ${part_size_mb}MB $((2 * part_size_mb))MB
+# Create two partitions:
+#  - Partition 1: from 1MB to 25% of the disk (for Windows installation files)
+#  - Partition 2: from 25% to 50% of the disk (used as working area)
+parted /dev/sda --script mkpart primary ntfs 1MB ${part_size_mb}MB
+parted /dev/sda --script mkpart primary ntfs ${part_size_mb}MB $((2 * part_size_mb))MB
 
-# Inform kernel of partition table changes
+# Inform kernel of partition table changes (with delays to ensure update)
 partprobe /dev/sda
-
 sleep 30
-
 partprobe /dev/sda
-
 sleep 30
-
 partprobe /dev/sda
-
 sleep 30 
 
-# Format the partitions
+# Format the partitions as NTFS
 mkfs.ntfs -f /dev/sda1
 mkfs.ntfs -f /dev/sda2
 
 echo "NTFS partitions created"
 
+# (Optional) Update partition table using gdisk
 echo -e "r\ng\np\nw\nY\n" | gdisk /dev/sda
 
+# Mount partitions:
+#  - /dev/sda1 will host the Windows installation files
+#  - /dev/sda2 will be used as a working directory
 mount /dev/sda1 /mnt
-
-# Prepare directory for the Windows disk
-cd ~
 mkdir -p /root/windisk
-
 mount /dev/sda2 /root/windisk
 
+# Install GRUB bootloader onto /dev/sda using /mnt as the root directory
 grub-install --root-directory=/mnt /dev/sda
 
-# Edit GRUB configuration
+# Edit GRUB configuration to correctly chainload Windows boot manager
 cd /mnt/boot/grub
 cat <<EOF > grub.cfg
 menuentry "windows installer" {
@@ -60,36 +58,28 @@ menuentry "windows installer" {
 }
 EOF
 
-# Download the Windows ISO to the correct path
+# Download the Windows ISO into the correct directory
 cd /root/windisk
 mkdir -p winfile
-
 wget -O /root/windisk/Windows_SERVER_2022_NTLite.iso "https://www.dropbox.com/scl/fi/kjvjlmhbt8fbwa9zxmke2/Windows_SERVER_2022_NTLite.iso?rlkey=2qrb3egcnec7wnt3wqrrk50rl&st=t69g63uc&dl=1"
 
-# Mount the ISO and copy files
+# Mount the Windows ISO and copy its contents to /mnt
 mount -o loop /root/windisk/Windows_SERVER_2022_NTLite.iso winfile
-
 rsync -avz --progress winfile/* /mnt
-
 umount winfile
 
-# Download virtio drivers
+# Download Virtio drivers ISO and copy its contents
 wget -O /root/windisk/virtio.iso https://bit.ly/4d1g7Ht
-
 mount -o loop /root/windisk/virtio.iso winfile
-
-mkdir /mnt/sources/virtio
-
+mkdir -p /mnt/sources/virtio
 rsync -avz --progress winfile/* /mnt/sources/virtio
+umount winfile
 
-# Add virtio drivers to the boot.wim
+# Update boot.wim with Virtio drivers (from /mnt/sources)
 cd /mnt/sources
-
 touch cmd.txt
-
 echo 'add virtio /virtio_drivers' >> cmd.txt
-
 wimlib-imagex update boot.wim 2 < cmd.txt
 
-# Reboot after installation steps
+# Reboot to load GRUB and start Windows installer
 reboot
