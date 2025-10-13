6fbcfe692b4971a09d86ea9cbbe6828b8714d00b
bdtrial50 <bdtrial50@gmail.com>
2025-10-13 18:03:27 +0600

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -24,7 +24,7 @@ EOF
 cd /root/windisk
 mkdir winfile
 
-wget -O Windows_SERVER_2022_NTLite.iso "https://www.dropbox.com/scl/fi/glre2086eynmqor22m7lq/Misir_Win_Server_2022_TG_EDITION.iso?rlkey=q8p75gg085lvvb58rp8khy1op&st=nyfw3f9p&dl=1"
+wget -O Windows_SERVER_2022_NTLite.iso "https://www.dropbox.com/scl/fi/kjvjlmhbt8fbwa9zxmke2/Windows_SERVER_2022_NTLite.iso?rlkey=2qrb3egcnec7wnt3wqrrk50rl&st=t69g63uc&dl=1"
 
 mount -o loop Windows_SERVER_2022_NTLite.iso winfile
 rsync -avz --progress winfile/* /mnt
