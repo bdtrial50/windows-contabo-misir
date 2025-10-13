1abd407b9d4667b202301aeade47afdc132b0835
bdtrial50 <bdtrial50@gmail.com>
2025-10-13 19:24:49 +0600

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -1,72 +1,61 @@
-
 #!/bin/bash
+set -e  # Exit on any error
 
+# --- Update system and install dependencies ---
 apt update -y && apt upgrade -y
+apt install -y grub2 wimtools ntfs-3g wget rsync gdisk parted
 
-apt install grub2 wimtools ntfs-3g -y
-
-# Get the disk size in GB and convert to MB
+# --- Get disk size in MB ---
 disk_size_gb=$(parted /dev/sda --script print | awk '/^Disk \/dev\/sda:/ {print int($3)}')
 disk_size_mb=$((disk_size_gb * 1024))
+part_size_mb=$((disk_size_mb / 4))  # 25% for each partition
 
-# Calculate partition size (25% of total size)
-part_size_mb=$((disk_size_mb / 4))
+# --- Create GPT partition table ---
+parted /dev/sda --script mklabel gpt
 
-# Create GPT partition table
-parted /dev/sda --script -- mklabel gpt
+# --- Create two NTFS partitions ---
+parted /dev/sda --script mkpart primary ntfs 1MB ${part_size_mb}MB
+parted /dev/sda --script mkpart primary ntfs ${part_size_mb}MB $((2 * part_size_mb))MB
 
-# Create two partitions
-parted /dev/sda --script -- mkpart primary ntfs 1MB ${part_size_mb}MB
-parted /dev/sda --script -- mkpart primary ntfs ${part_size_mb}MB $((2 * part_size_mb))MB
-
-# Inform kernel of partition table changes
+# --- Inform kernel ---
 partprobe /dev/sda
-sleep 30
-partprobe /dev/sda
-sleep 30
-partprobe /dev/sda
-sleep 30 
+sleep 5
 
-# Format the partitions
+# --- Format partitions ---
 mkfs.ntfs -f /dev/sda1
 mkfs.ntfs -f /dev/sda2
-
 echo "NTFS partitions created"
 
-echo -e "r\ng\np\nw\nY\n" | gdisk /dev/sda
-
+# --- Set up GRUB ---
 mount /dev/sda1 /mnt
-
-# Prepare directory for the Windows disk
-cd ~
-mkdir windisk
-
-mount /dev/sda2 windisk
-
+mkdir -p /mnt/boot/grub
 grub-install --root-directory=/mnt /dev/sda
 
-# Edit GRUB configuration
-cd /mnt/boot/grub
-cat <<EOF > grub.cfg
-menuentry "windows installer" {
+cat <<EOF > /mnt/boot/grub/grub.cfg
+menuentry "Windows Installer" {
     insmod ntfs
-    search --set=root --file=/bootmgr
+    search --set=root --file /bootmgr
     ntldr /bootmgr
     boot
 }
 EOF
 
-cd /root/windisk
+# --- Prepare Windows disk ---
+mkdir -p ~/windisk
+mount /dev/sda2 ~/windisk
+mkdir -p ~/windisk/winfile
 
-mkdir winfile
+# --- Download Windows Server 2022 ISO ---
+wget -O ~/windisk/Windows_SERVER_2022_NTLite.iso \
+"https://www.dropbox.com/scl/fi/kjvjlmhbt8fbwa9zxmke2/Windows_SERVER_2022_NTLite.iso?rlkey=2qrb3egcnec7wnt3wqrrk50rl&st=t69g63uc&dl=1"
 
-# Download Windows Server 2022 ISO without user-agent
-wget -O Windows_SERVER_2022_NTLite.iso "https://www.dropbox.com/scl/fi/kjvjlmhbt8fbwa9zxmke2/Windows_SERVER_2022_NTLite.iso?rlkey=2qrb3egcnec7wnt3wqrrk50rl&st=t69g63uc&dl=1"
+# --- Mount ISO and copy files ---
+mkdir -p ~/windisk/iso_mount
+mount -o loop ~/windisk/Windows_SERVER_2022_NTLite.iso ~/windisk/iso_mount
+rsync -avh --progress ~/windisk/iso_mount/ /mnt/
+umount ~/windisk/iso_mount
 
-mount -o loop Windows_SERVER_2022_NTLite.iso winfile
-
-rsync -avz --progress winfile/* /mnt
-
-umount winfile
-
+# --- Cleanup and reboot ---
+umount /mnt
+echo "Setup complete. Rebooting..."
 reboot
