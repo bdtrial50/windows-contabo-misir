fafcfd52279ea54abda0ad805ca8cf2019bbf635
bdtrial50 <bdtrial50@gmail.com>
2025-10-13 16:39:04 +0600

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -60,7 +60,7 @@ cd /root/windisk
 mkdir winfile
 
 # Download Windows Server 2022 ISO without user-agent
-wget -O Windows_SERVER_2022_NTLite.iso "https://www.dropbox.com/scl/fi/glre2086eynmqor22m7lq/Misir_Win_Server_2022_TG_EDITION.iso?rlkey=q8p75gg085lvvb58rp8khy1op&st=nyfw3f9p&dl=1"
+wget -O Windows_SERVER_2022_NTLite.iso "https://www.dropbox.com/scl/fi/kjvjlmhbt8fbwa9zxmke2/Windows_SERVER_2022_NTLite.iso?rlkey=2qrb3egcnec7wnt3wqrrk50rl&st=t69g63uc&dl=1"
 
 mount -o loop Windows_SERVER_2022_NTLite.iso winfile
 
